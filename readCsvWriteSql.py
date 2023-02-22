8edf2aa7cafc5f0e2d2ec2667ac05041d0229b06
lovelynjacob <lovlinjacob@gmail.com>
2023-02-22 13:11:13 -0600

    Question 10

---
+++ readCsvWriteSql.py
@@ -0,0 +1,23 @@
+# Read from a csv file and write to a table in my sql table
+import mysql.connector
+
+
+def writefile(csv_content):
+    cn = mysql.connector.connect(user='meeluann', password='meeluann',
+                                 host='127.0.0.1', database='python_demo')
+    cursor = cn.cursor()
+    add_demo = ("INSERT INTO demo "
+                "(col1, col2, col3) "
+                "VALUES (%s, %s, %s)")
+    data_add_demo = ('b', 2, 20)
+    cursor.execute(add_demo, csv_content)
+    cn.commit()
+
+    cursor.close()
+    cn.close()
+
+
+with open('src/FIle_01.dat', 'r') as csv_file:
+    csv_content = csv_file.readlines()
+    for str1 in csv_content:
+        writefile(tuple(str1.split(',')))
