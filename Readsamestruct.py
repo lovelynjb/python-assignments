5e1525090ae60135ca6860da71aea117cb784316
lovelynjb <126007678+lovelynjb@users.noreply.github.com>
2023-02-24 13:17:45 -0600

    Update Readsamestruct.py

--- Readsamestruct.py
+++ Readsamestruct.py
@@ -1,6 +1,5 @@
 # 1. Read from multiple files with same structure /src folder in your m/c
-# 2. load into a single file with same structure /tgt folder in your m/c
-# 3. Convert the single csv file to a parquet format in /tgt folder in your m/c
+
 import glob
 
 for file_ext in ['src/*.txt', 'src/*.dat']:
